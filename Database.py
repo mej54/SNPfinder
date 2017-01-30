814f63edde5f685563f8446a2a8fb5ffe498a564
mej54 <mollyejohnson1@gmail.com>
2017-01-30 18:19:26 -0500

    updated api access

--- Database.py
+++ Database.py
@@ -1,15 +1,20 @@
 from pip._vendor.distlib.compat import raw_input
 import json
-import requests
+import urllib2
 
-def accessOMIM():
-    url = 'http://api.omim.org/api/entry?mimNumber=100100'
-    apikey = 'gZuFDYrRTlKEPLkePeEBog'
-    r = requests.get(url)
-    r.status_code
+def accessOMIM(query):
+    gene = query
+    url = 'https://api.omim.org/api/entry/search?search='
+    #https://api.omim.org/api/entry/search?search=pik3ca&filter=&fields=&retrieve=&start=0&limit=10&sort=&operator=&include=allelicVariantList&include=clinicalSynopsis&format=json
+    filters = '&filter=&fields=&retrieve=&start=0&limit=10&sort=&operator=&include=allelicVariantList&include=clinicalSynopsis&format=json' 
+    apikey = '&apiKey=gZuFDYrRTlKEPLkePeEBog'
+    final_url = url + filters + apikey
+    json_obj = urllib2.urlopen(final_url)
+    data = json.load(json_obj)
+    print data
 
 def inputUser():
     geneSymbol = raw_input('Enter approved gene symbol of interest:')
- 
+    accessOMIM(geneSymbol)
     
 inputUser()
